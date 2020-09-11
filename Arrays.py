ea8fdd4bd15f9c8881195a1cca71cb7b8bf587e9
Herve Kapuya <Herventh@gmail.com>
2020-09-11 05:27:09 +0200

    Reformating elements

--- Arrays.py
+++ Arrays.py
@@ -1,25 +1,24 @@
-
 import array as arr
 
-Item = arr.array('i',[3,9,8,4,1,4,1])
+Item = arr.array('i', [1, 2, 3, 4, 5, 6])
+
 
 # Defining our Function named ItemDuplicate
 
-def ItemDuplicate (Item) :
-
+def ItemDuplicate(Item):
     # Verification doublons
 
-       if len(Item) == len(set(Item)) :
-           return False
-       else:
-           return True
+    if len(Item) == len(set(Item)):
+        return False
+    else:
+        return True
+
 
 # Declaring our list
 
 result = ItemDuplicate(Item)
 
 if result:
-
     print('Yes, there is some Duplicate Item')
 else:
     print('Nothing Duplicate Item inside')
